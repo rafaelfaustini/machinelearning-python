72c2d32e7bbce844e6d16b132c06c2cf9af0933b
Rafael Faustini <contato@rafaelfaustini.com.br>
2020-08-04 13:14:21 -0300

    Dog or cat

---
+++ cnn/CatorDog/init.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+
+
+with tf.device('/gpu:0'):
+    model = Sequential()
+    model.add(Conv2D(32, (3,3), input_shape = (64, 64, 3), activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    
+    model.add(Conv2D(32, (3,3), input_shape = (64, 64, 3), activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    
+    model.add(Flatten())
+    
+    model.add(Dense(units =  128, activation = 'relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units = 128, activation = 'relu'))
+    model.add(Dense(units = 1, activation = 'sigmoid'))
+    
+    model.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                  metrics = ['accuracy'])
+    
+    gerador_treinamento = ImageDataGenerator(rescale = 1./255,
+                                             rotation_range = 7,
+                                             horizontal_flip = True,
+                                             shear_range = 0.2,
+                                             height_shift_range = 0.07,
+                                             zoom_range = 0.2)
+    gerador_teste = ImageDataGenerator(rescale = 1./255)
+
+    
+    base_treinamento = gerador_treinamento.flow_from_directory('dataset/training_set',
+                                                               target_size = (64, 64),
+                                                               batch_size = 32,
+                                                               class_mode = 'binary')
+    base_teste = gerador_teste.flow_from_directory('dataset/test_set',
+                                                   target_size = (64, 64),
+                                                   batch_size = 32,
+                                                   class_mode = 'binary')
+    model.fit_generator(base_treinamento, steps_per_epoch= 4000/ 32,
+                        epochs = 5, validation_data = base_teste,
+                        validation_steps = 1000 / 32)
+                    
